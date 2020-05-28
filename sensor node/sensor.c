5643dadd2abf7c1e4eff66a32c57a20f233c18ae
Benoît Michel <b.michel@student.uclouvain.be>
2020-05-28 11:58:10 +0200

    Sensor optimization

--- sensor node/sensor.c
+++ sensor node/sensor.c
@@ -1,124 +1,124 @@
+/*
+	LINGI2146 Mobile and Embedded Computing : Project1
+	Author : Benoît Michel
+	Date : May 2020
+*/
+
 #include "contiki.h"
-#include "net/rime/rime.h"
 #include "random.h"
 #include "dev/button-sensor.h"
 #include "dev/leds.h"
-#include <stdio.h>
-#include <limits.h>
-#include "sys/ctimer.h"
-#include "sys/timer.h"
-#include "cc2420.h"
-#include "cc2420_const.h"
 #include "lib/list.h"
 #include "lib/memb.h"
+#include "net/rime/rime.h"
+#include "sys/timer.h"
+#include "sys/ctimer.h"
+#include "cc2420.h"
+#include "cc2420_const.h"
+
+#include <stdio.h>
+#include <limits.h>
 #include <stdbool.h>
 
-
-//DEFINE
-#define seconds_before_routing_info 120 //TODO change to something like 120 after testing
-#define seconds_before_sending_measurement 60 //TODO change to something like 60 after testing
+#define MAX_HISTORY 10
+#define MAX_CHILDREN 100
 #define MAX_RETRANSMISSIONS 10
-#define NUM_HISTORY_ENTRIES 10
-#define MAX_CHILDREN 128
+#define seconds_before_routing_info 120
+#define seconds_before_sending_measurement 60
 
 
-//STRUCT DEF
-/**
-*	senderAdr: address from the sensor sending datasize
-*	destAddr: usually the border node (or sensor if open/close command)
-*	status: contain the temperature value
-*	rank: contain the rank of the sensor (not used for now)
-*	option: used to define the content of the message
-*	datasize: size of the data
-*	Data: contain the data of the message eg: children list
-**/
+// Structures definition
+typedef struct Broadcast broadcast_struct;
+struct Broadcast
+{
+	short rank;                             // rank of the node
+	linkaddr_t sendAddr;                    // address of the node sending the message
+	uint8_t option;                         // type of message
+};
+
+typedef struct Runicast runicast_struct;
 struct Runicast
 {
-	linkaddr_t senderAdr;
-	linkaddr_t destAddr;
-	short status;
-	short valve_status;
-	short rank;
-	uint8_t option;
-	linkaddr_t child_lost;
+	short rank;                             // rank of the node
+	short temp;                             // temperature value read by the sensor
+	short valve_status;                     // current state of the valve : closed(0) or open(1)
+	linkaddr_t sendAddr;                    // address of the node sending the message
+	linkaddr_t destAddr;                    // address of the node targeted to receive the message
+	linkaddr_t child_lost;                  //
+	uint8_t option;                         // type of message
 };
-typedef struct Runicast runicast_struct;
 
+typedef struct Children children_struct;
+struct Children
+{
+	linkaddr_t address;                     // address of the child
+	linkaddr_t next_hop;                    // nexthop
+	clock_time_t last_update;               // last update of the children
+	struct children_struct *next;           // next child
+};
+
+typedef struct History history_struct;
+struct History {
+	uint8_t seq;                           // sequence number
+	linkaddr_t addr;                       // address of the node
+	struct history_struct *next;           // next entry in the history
+};
+
+
+// Enumerations definition
 enum{
 	SENSOR_INFO,
 	OPENING_VALVE,
-	FLUSH_CHILDREN,
-	CHILDREN_LOST
+	SAVE_CHILDREN,
+	LOST_CHILDREN
 };
 
-
-/*
-* Detects duplicate callbacks at receiving nodes.
-* Duplicates appear when ack messages are lost. */
-struct history_entry {
-	struct history_entry *next;
-	linkaddr_t addr;
-	uint8_t seq;
-};
-LIST(history_table);
-MEMB(history_mem, struct history_entry, NUM_HISTORY_ENTRIES);
-
-
-struct Broadcast
-{
-	short rank;
-	linkaddr_t srcAdr;
-	uint8_t option;
-};
-typedef struct Broadcast broadcast_struct;
 enum {
 	BROADCAST_INFO,
 	BROADCAST_REQUEST
 };
 
-struct Children
-{
-	struct Children *next;
-	linkaddr_t address;
-	linkaddr_t next_hop;
-	clock_time_t last_update;
-};
 
+// Memory blocks allocation
+LIST(history_table);
+MEMB(history_mem, history_struct, MAX_HISTORY);
 
-//static variable
-static short _rank;
-static linkaddr_t parent_addr;
-static int rssi_parent;
-
-MEMB(children_memb, struct Children, MAX_CHILDREN);
 LIST(children_list);
+MEMB(children_memb, children_struct, MAX_CHILDREN);
 
-static struct broadcast_conn broadcast;
-static struct runicast_conn ruc;
+
+// Static variables definition
+static int rssi_parent;
+static short static_rank;
+static linkaddr_t parent_addr;
 static short valve_is_open = 0;
+
+// Static structures definition
+static struct broadcast_conn broadcast;
+static struct runicast_conn runicast;
 static struct ctimer ctimer_valve_reset;
 
 
 /*---------------------------------------------------------------------------*/
-PROCESS(sensor_process_runicast, "sensor runicast");
-PROCESS(broadcast_routing, "broadcast routing");
+PROCESS(sensor_process_runicast, "[sensor node] + runicast execution");
+PROCESS(broadcast_routing, "[sensor node] + broadcast routing");
 AUTOSTART_PROCESSES(&sensor_process_runicast, &broadcast_routing);
 /*---------------------------------------------------------------------------*/
 
 
 static void closing_valve_timeout(){
 	valve_is_open=0;
-	printf("10 minutes have passed: closing valve\n");
+	printf("[sensor node] - Closing valve after 10 minutes open\n");
 }
 
 
 /**
 * //TODO remove the broadcast struct and just use a pointer to the rank
 * broadcast_recv is called when a routing information from a neighbouring node is recieved
-* it will decide if the node sending the broadcast would be a better suited parent that the current one 
-* Parameters: 
+* it will decide if the node sending the broadcast would be a better suited parent that the current one
+* Parameters:
 * struct broadcast_conn *c : a pointer to the structure containing the rank and the adress of the sending node
-* const linkaddr_t *from : the adress of the sender 
+* const linkaddr_t *from : the adress of the sender
 **/
 //TODO !!!!RSSI REMOVED FOR TEST PURPOSES
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
@@ -130,23 +130,23 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 		rss_val = cc2420_last_rssi;
 		rss_offset=-45;
 		rss_val = rss_val + rss_offset;
-		//printf("Routing information recieved from: src %d.%d with rank %d rss value: %d\n", arrival->srcAdr.u8[0],arrival->srcAdr.u8[1] , arrival->rank, rss_val );
+		//printf("Routing information recieved from: src %d.%d with rank %d rss value: %d\n", arrival->sendAddr.u8[0],arrival->sendAddr.u8[1] , arrival->rank, rss_val );
 		//TODO change the +1 by the quality of communication or smth
-		if(arrival->rank < _rank -1 && rss_val > rssi_parent){ //cant add 1 on the left because it would overflow, so substract one on right side (since always > 0)
-			_rank = arrival->rank +1;
-			parent_addr.u8[0] = (arrival->srcAdr).u8[0];
-			parent_addr.u8[1] = (arrival->srcAdr).u8[1];
+		if(arrival->rank < static_rank -1 && rss_val > rssi_parent){ //cant add 1 on the left because it would overflow, so substract one on right side (since always > 0)
+			static_rank = arrival->rank +1;
+			parent_addr.u8[0] = (arrival->sendAddr).u8[0];
+			parent_addr.u8[1] = (arrival->sendAddr).u8[1];
 			rssi_parent = rss_val;
-			printf("\t parent changed, new ranking : new rank: %d, parent address %d.%d \n",_rank, (parent_addr).u8[0],(arrival->srcAdr).u8[1]);
-		} 
+			printf("\t parent changed, new ranking : new rank: %d, parent address %d.%d \n",static_rank, (parent_addr).u8[0],(arrival->sendAddr).u8[1]);
+		}
 	}
-	else if(arrival->option == BROADCAST_REQUEST && _rank != SHRT_MAX){
-		//printf("Sending routing info via broadcast rank: %d\n", _rank);
+	else if(arrival->option == BROADCAST_REQUEST && static_rank != SHRT_MAX){
+		//printf("Sending routing info via broadcast rank: %d\n", static_rank);
 		broadcast_struct message;
-		message.rank = _rank; 
-		message.srcAdr.u8[0] = linkaddr_node_addr.u8[0]; 
-		message.srcAdr.u8[1] = linkaddr_node_addr.u8[1];
-		packetbuf_copyfrom(&message ,sizeof(message)); 
+		message.rank = static_rank;
+		message.sendAddr.u8[0] = linkaddr_node_addr.u8[0];
+		message.sendAddr.u8[1] = linkaddr_node_addr.u8[1];
+		packetbuf_copyfrom(&message ,sizeof(message));
 		broadcast_send(&broadcast);
 	}
 	else{
@@ -161,7 +161,7 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 	static signed char rss_offset = -45;
 	runicast_struct* arrival = packetbuf_dataptr();
 	/* OPTIONAL: Sender history */
-	struct history_entry *e = NULL;
+	struct history_struct *e = NULL;
 	for(e = list_head(history_table); e != NULL; e = e->next) {
 		if(linkaddr_cmp(&e->addr, from)) {
 			break;
@@ -186,24 +186,17 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 		e->seq = seqno;
 	}
 
-	printf("runicast message recieved from %d.%d | originating from %d.%d | value %d \n",from->u8[0], from->u8[1],arrival->senderAdr.u8[0],arrival->senderAdr.u8[1],arrival->status);
-
-	/*
-	//FOR debugging, root node don't transmit received messages
-	if(linkaddr_node_addr.u8[0] == 1){
-		return; //TODO remove, for testing only
-	}
-*/
+	printf("runicast message recieved from %d.%d | originating from %d.%d | value %d \n",from->u8[0], from->u8[1],arrival->sendAddr.u8[0],arrival->sendAddr.u8[1],arrival->temp);
 
 	if(arrival->option == SENSOR_INFO){
-		printf("sensor info received from %d.%d | originating from %d.%d, transmitting to parent: %d.%d \n", from->u8[0], from->u8[1],arrival->senderAdr.u8[0],arrival->senderAdr.u8[1], parent_addr.u8[0], parent_addr.u8[1]);
+		printf("sensor info received from %d.%d | originating from %d.%d, transmitting to parent: %d.%d \n", from->u8[0], from->u8[1],arrival->sendAddr.u8[0],arrival->sendAddr.u8[1], parent_addr.u8[0], parent_addr.u8[1]);
 		linkaddr_copy(&arrival->destAddr, &parent_addr);
 		packetbuf_copyfrom(arrival ,sizeof(runicast_struct));
-		runicast_send(&ruc, &parent_addr, MAX_RETRANSMISSIONS);
+		runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
 
-		struct Children *n;
+		children_struct *n;
 		for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
-			if(linkaddr_cmp(&n->address, &arrival->senderAdr)) {
+			if(linkaddr_cmp(&n->address, &arrival->sendAddr)) {
 				n->last_update = clock_time();
 				break;
 			}
@@ -214,7 +207,7 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 			if(n == NULL){
 				return;
 			}
-			linkaddr_copy(&n->address, &arrival->senderAdr);
+			linkaddr_copy(&n->address, &arrival->sendAddr);
 			linkaddr_copy(&n->next_hop, from);
 			n->last_update = clock_time();
 			list_add(children_list, n);
@@ -226,7 +219,7 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 			printf("Opening valve\n");
 		}
 		else{
-			struct Children *n;
+			children_struct *n;
 			bool found = false;
 			for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 				if(linkaddr_cmp(&n->address, &arrival->destAddr)) {
@@ -235,11 +228,11 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 				}
 			}
 			packetbuf_copyfrom(arrival ,sizeof(runicast_struct));
-			if(found==true){ 
-				runicast_send(&ruc, &n->next_hop, MAX_RETRANSMISSIONS);
+			if(found==true){
+				runicast_send(&runicast, &n->next_hop, MAX_RETRANSMISSIONS);
 			}
 			else{ //don't know where to send it, -> send to parent
-				runicast_send(&ruc, &parent_addr, MAX_RETRANSMISSIONS);
+				runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
 			}
 		}
 	}
@@ -251,7 +244,7 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 			ctimer_set(&ctimer_valve_reset, 600 * CLOCK_SECOND, closing_valve_timeout, NULL);
 		}
 		else{
-			struct Children *n;
+			children_struct *n;
 			bool found = false;
 			for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 				if(linkaddr_cmp(&n->address, &arrival->destAddr)) {
@@ -261,31 +254,31 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 			}
 
 			packetbuf_copyfrom(arrival ,sizeof(runicast_struct));
-			if(found==true){ 
-				runicast_send(&ruc, &n->next_hop, MAX_RETRANSMISSIONS);
+			if(found==true){
+				runicast_send(&runicast, &n->next_hop, MAX_RETRANSMISSIONS);
 			}
 			else{ //don't know where to send it, -> send to parent
-				runicast_send(&ruc, &parent_addr, MAX_RETRANSMISSIONS);
+				runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
 			}
 		}
 	}*/
-	else if(arrival->option == FLUSH_CHILDREN){
+	else if(arrival->option == SAVE_CHILDREN){
 		printf("flushing info received\n");
 		printf("parent lost\n");
-		_rank = SHRT_MAX;
+		static_rank = SHRT_MAX;
 		rssi_parent= -SHRT_MAX;
-		struct Children *n;
+		children_struct *n;
 		for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 			if(linkaddr_cmp(&n->address, &n->next_hop)){
 				arrival->destAddr = n->address;
 				packetbuf_copyfrom(arrival ,sizeof(runicast_struct));
-				runicast_send(&ruc, &n->next_hop, MAX_RETRANSMISSIONS);
+				runicast_send(&runicast, &n->next_hop, MAX_RETRANSMISSIONS);
 			}
 			list_remove(children_list, n);
 		}
 	}
-	else if(arrival->option == CHILDREN_LOST){
-		struct Children *n;
+	else if(arrival->option == LOST_CHILDREN){
+		children_struct *n;
 		for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 			if(linkaddr_cmp(&n->address, &arrival->child_lost)){
 				list_remove(children_list, n);
@@ -294,7 +287,7 @@ static void recv_ruc(struct runicast_conn *c, const linkaddr_t *from, uint8_t se
 
 		}
 		packetbuf_copyfrom(arrival ,sizeof(runicast_struct));
-		runicast_send(&ruc, &parent_addr, MAX_RETRANSMISSIONS);
+		runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
 	}
 }
 
@@ -307,39 +300,39 @@ static void timedout_runicast(struct runicast_conn *c, const linkaddr_t *to, uin
 	//if parent is not reachable, we forget it
 	if(linkaddr_cmp(to, &parent_addr)){
 		printf("parent lost\n");
-		_rank = SHRT_MAX;
+		static_rank = SHRT_MAX;
 		rssi_parent= -SHRT_MAX;
-		struct Children *n;
+		children_struct *n;
 		runicast_struct flush_message;
-		linkaddr_copy(&(&flush_message)->senderAdr, &linkaddr_node_addr);
-		(&flush_message)->option = FLUSH_CHILDREN;
+		linkaddr_copy(&(&flush_message)->sendAddr, &linkaddr_node_addr);
+		(&flush_message)->option = SAVE_CHILDREN;
 		for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 			if(linkaddr_cmp(&n->address, &n->next_hop)){
 				linkaddr_copy(&(&flush_message)->destAddr, &n->address);
 				packetbuf_copyfrom(&flush_message ,sizeof(runicast_struct));
-				runicast_send(&ruc, &n->next_hop, MAX_RETRANSMISSIONS);
+				runicast_send(&runicast, &n->next_hop, MAX_RETRANSMISSIONS);
 			}
 			list_remove(children_list, n);
 		}
 	}
 	//in case children is no more reachable
 	else{
-		struct Children *n;
+		children_struct *n;
 		bool found = false;
 		for(n = list_head(children_list); n != NULL; n = list_item_next(n)) {
 			if(linkaddr_cmp(&n->address, to)){
 				list_remove(children_list, n);
 				found = true;
 				break;
-			}	
+			}
 		}
 
-		if (found==true) { 
+		if (found==true) {
 			runicast_struct lost_msg;
-			linkaddr_copy(&(&lost_msg)->senderAdr, &linkaddr_node_addr);
-			(&lost_msg)->option = CHILDREN_LOST;
+			linkaddr_copy(&(&lost_msg)->sendAddr, &linkaddr_node_addr);
+			(&lost_msg)->option = LOST_CHILDREN;
 			packetbuf_copyfrom(&lost_msg ,sizeof(runicast_struct));
-			runicast_send(&ruc, &n->next_hop, MAX_RETRANSMISSIONS);
+			runicast_send(&runicast, &n->next_hop, MAX_RETRANSMISSIONS);
 		}
 	}
 }
@@ -366,10 +359,10 @@ short collect_measurement(){
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(sensor_process_runicast, ev, data){
-	PROCESS_EXITHANDLER(runicast_close(&ruc);)
+	PROCESS_EXITHANDLER(runicast_close(&runicast);)
 	PROCESS_BEGIN();
 	random_init(linkaddr_node_addr.u8[0]); //to use a different seed per node to generate random measurement, I use their IP address
-	runicast_open(&ruc, 144, &runicast_callbacks);
+	runicast_open(&runicast, 144, &runicast_callbacks);
 
 	while(1) {
 		static struct etimer et;
@@ -377,17 +370,17 @@ PROCESS_THREAD(sensor_process_runicast, ev, data){
 		etimer_set(&et, CLOCK_SECOND * seconds_before_sending_measurement);
 
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-		if(_rank != SHRT_MAX){
+		if(static_rank != SHRT_MAX){
 			printf("sending runicast value to parent %d.%d\n", parent_addr.u8[0], parent_addr.u8[1]);
-				linkaddr_copy(&(&msg)->senderAdr, &linkaddr_node_addr);
+				linkaddr_copy(&(&msg)->sendAddr, &linkaddr_node_addr);
 				linkaddr_copy(&(&msg)->destAddr, &parent_addr);
-				msg.status = collect_measurement();
-				msg.rank = _rank;
+				msg.temp = collect_measurement();
+				msg.rank = static_rank;
 				msg.valve_status = valve_is_open;
 				msg.option = SENSOR_INFO;
 
 			packetbuf_copyfrom(&msg, sizeof(msg));
-			runicast_send(&ruc, &parent_addr, MAX_RETRANSMISSIONS);
+			runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
 		}
 	}
 	PROCESS_END();
@@ -401,10 +394,10 @@ PROCESS_THREAD(broadcast_routing, ev, data){
 	PROCESS_BEGIN();
 	broadcast_open(&broadcast, 129, &broadcast_call);
 
-	_rank = SHRT_MAX;
+	static_rank = SHRT_MAX;
 	rssi_parent = -SHRT_MAX;
 
-	//if(linkaddr_node_addr.u8[0]==1){_rank = 0; printf("fake root\n");} //TODO remove, just for testing without border router
+	//if(linkaddr_node_addr.u8[0]==1){static_rank = 0; printf("fake root\n");} //TODO remove, just for testing without border router
 
 	while(1) {
 		static struct etimer et;
@@ -412,16 +405,16 @@ PROCESS_THREAD(broadcast_routing, ev, data){
 		etimer_set(&et, CLOCK_SECOND * seconds_before_routing_info);
 
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-		if(_rank != SHRT_MAX){
-			//printf("Sending routing info via broadcast rank: %d\n", _rank);
+		if(static_rank != SHRT_MAX){
+			//printf("Sending routing info via broadcast rank: %d\n", static_rank);
 			broadcast_struct message;
-			message.rank = _rank; 
+			message.rank = static_rank;
 			message.option = BROADCAST_INFO; //modified
-			message.srcAdr.u8[0] = linkaddr_node_addr.u8[0]; 
-			message.srcAdr.u8[1] = linkaddr_node_addr.u8[1];
-			packetbuf_copyfrom(&message ,sizeof(message)); 
+			message.sendAddr.u8[0] = linkaddr_node_addr.u8[0];
+			message.sendAddr.u8[1] = linkaddr_node_addr.u8[1];
+			packetbuf_copyfrom(&message ,sizeof(message));
 			broadcast_send(&broadcast);
-		} 
+		}
 		else{
 			printf("Not sending routing info, not connected to the network\n");
 		}
@@ -429,4 +422,3 @@ PROCESS_THREAD(broadcast_routing, ev, data){
 	PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-
